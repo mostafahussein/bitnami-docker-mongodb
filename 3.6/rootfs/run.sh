ff61ad9c4d2cf7c68e1f2be358d52ca1668ff85d
Mostafa Hussein <mostafa.hussein91@gmail.com>
2018-08-29 15:57:10 +0200

    introduce custom file initialization

--- 3.6/rootfs/run.sh
+++ 3.6/rootfs/run.sh
@@ -15,5 +15,43 @@ fi
 # log output to stdout
 sed -i 's/path: .*\/mongodb.log/path: /' /opt/bitnami/mongodb/conf/mongodb.conf
 
+
+# allow running custom initialization scripts
+if [[ -n $(find /docker-entrypoint-initdb.d/ -type f -regex ".*\.\(sh\|js\)") ]] && [[ ! -f /opt/bitnami/mongodb/.user_scripts_initialized ]] ; then
+    exec gosu ${USER} ${EXEC} ${ARGS} &
+
+    echo "==> Loading user files from /docker-entrypoint-initdb.d";
+    if [[ -n "$MONGODB_ROOT_PASSWORD" ]]; then
+	 echo "1"
+	sleep 20
+        mongo=( mongo admin --username root --password $MONGODB_ROOT_PASSWORD --host localhost --quiet )
+    else
+	echo "2"
+	sleep 20
+        mongo=( mongo admin --host localhost --quiet )
+    fi
+
+    for f in /docker-entrypoint-initdb.d/*; do
+        case "$f" in
+            *.sh)
+                if [ -x "$f" ]; then
+                    echo "Executing $f"; "$f"
+                else
+                    echo "Sourcing $f"; . "$f"
+                fi
+                ;;
+            *.js)   echo "Executing $f"; "${mongo[@]}" "$f"; echo ;;
+            *)      echo "Ignoring $f" ;;
+        esac
+    done
+    touch /opt/bitnami/mongodb/.user_scripts_initialized
+    pidfile="/opt/bitnami/mongodb/tmp/mongodb.pid"
+    dbpath="/opt/bitnami/mongodb/data/db"
+    if ! gosu "${USER}" "${EXEC}" --dbpath="$dbpath" --pidfilepath="$pidfile" --shutdown || ! rm -f "$pidfile"; then
+        echo >&2 'MongoDB init process failed.'
+        exit 1
+    fi
+fi
+
 info "Starting ${DAEMON}..."
 exec gosu ${USER} ${EXEC} ${ARGS}
