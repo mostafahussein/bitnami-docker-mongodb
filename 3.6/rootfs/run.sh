7a5c88876b59c8f0f0df44c1b1f6dfa9ccd12b57
Mostafa Hussein <mostafa.hussein91@gmail.com>
2018-08-30 17:18:37 +0200

    check if Mongo ready for connections
    Signed-off-by: Mostafa Hussein <mostafa.hussein91@gmail.com>

--- 3.6/rootfs/run.sh
+++ 3.6/rootfs/run.sh
@@ -19,15 +19,38 @@ sed -i 's/path: .*\/mongodb.log/path: /' /opt/bitnami/mongodb/conf/mongodb.conf
 # allow running custom initialization scripts
 if [[ -n $(find /docker-entrypoint-initdb.d/ -type f -regex ".*\.\(sh\|js\)") ]] && [[ ! -f /opt/bitnami/mongodb/.user_scripts_initialized ]] ; then
     exec gosu ${USER} ${EXEC} ${ARGS} &
+    pidfile="/opt/bitnami/mongodb/tmp/mongodb.pid"
+    dbpath="/opt/bitnami/mongodb/data/db"
+
+    # check to see that our "mongod" actually did start up
+    tries=30
+    while true; do
+	sleep 1
+        if ! { [ -s "$pidfile" ] && ps "$(< "$pidfile")" &> /dev/null; }; then
+	    # bail ASAP if "mongod" isn't even running
+	    echo >&2
+	    echo >&2 "error: ${DAEMON} does not appear to have stayed running -- perhaps it had an error?"
+	    echo >&2
+	    exit 1
+	fi
+	if mongo 'admin' --eval 'quit(0)' &> /dev/null; then
+	    # success!
+	    break
+	fi
+	(( tries-- ))
+	if [ "$tries" -le 0 ]; then
+	    echo >&2
+	    echo >&2 "error: ${DAEMON} does not appear to have accepted connections quickly enough -- perhaps it had an error?"
+	    echo >&2
+	    exit 1
+	fi
+	sleep 1
+    done
 
     echo "==> Loading user files from /docker-entrypoint-initdb.d";
     if [[ -n "$MONGODB_ROOT_PASSWORD" ]]; then
-	 echo "1"
-	sleep 20
         mongo=( mongo admin --username root --password $MONGODB_ROOT_PASSWORD --host localhost --quiet )
     else
-	echo "2"
-	sleep 20
         mongo=( mongo admin --host localhost --quiet )
     fi
 
@@ -45,8 +68,6 @@ if [[ -n $(find /docker-entrypoint-initdb.d/ -type f -regex ".*\.\(sh\|js\)") ]]
         esac
     done
     touch /opt/bitnami/mongodb/.user_scripts_initialized
-    pidfile="/opt/bitnami/mongodb/tmp/mongodb.pid"
-    dbpath="/opt/bitnami/mongodb/data/db"
     if ! gosu "${USER}" "${EXEC}" --dbpath="$dbpath" --pidfilepath="$pidfile" --shutdown || ! rm -f "$pidfile"; then
         echo >&2 'MongoDB init process failed.'
         exit 1
